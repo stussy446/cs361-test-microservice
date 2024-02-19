773410322d2c57be1b26b83c0f770022d66e8fd9
Steve Rector <steverector27@gmail.com>
2024-02-19 12:00:04 -0800

    Add bootstrap, setup card template

--- cs361-test-microservice/src/components/Card.jsx
+++ cs361-test-microservice/src/components/Card.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <>
+      <div className="card" styles="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Card;
