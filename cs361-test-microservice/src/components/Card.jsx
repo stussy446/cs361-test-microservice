c34768fced8336edd6faf244d6c5b508b72f32c4
Steve Rector <steverector27@gmail.com>
2024-02-19 18:15:36 -0800

    Successfully showing cards

--- cs361-test-microservice/src/components/Card.jsx
+++ cs361-test-microservice/src/components/Card.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
 const Card = ({ card }) => {
+  const { name, desc, type, race, attribute } = card;
   return (
     <>
       <div className="card" styles="width: 18rem;">
         <img
           src={card.card_images[0].image_url_small}
-          className="card-img-top"
+          className="card-img-top photo"
           alt="..."
         />
         <div className="card-body">
-          <h5 className="card-title">Placeholder</h5>
-          <p className="card-text">description</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{desc}</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">Type: {type}</li>
+          <li className="list-group-item">Race: {race}</li>
+          <li className="list-group-item">Main Attribute: {attribute}</li>
         </ul>
       </div>
     </>
